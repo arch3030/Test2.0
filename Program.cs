c04a1ccb1caf516fc2d358c8ed2baac761d4f517
arch3030 <78534135+arch3030@users.noreply.github.com>
2021-03-28 02:15:38 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _28_03_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            Console.Write("Input a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input b : ");
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            
+            while(x != y)
+            {
+                if(x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
